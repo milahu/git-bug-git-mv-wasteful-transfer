8db1d3e7f205ff0e737f1b9ba48daff25f7076f7
Milan Hauth <milahu@gmail.com>
2024-02-25 19:05:13 +0100

    cleanup

--- repro.sh
+++ repro.sh
@@ -1,218 +1,69 @@
 #!/usr/bin/env bash
 
-# no. this works as expected
-# only change the basename
-olddir_depth=0; newdir_depth=$olddir_depth; change_basename=true
+tempdir=$(mktemp -d /run/user/$UID/git-mv-bug.XXXXXXXXXX)
+cd $tempdir
 
-# no. this works as expected
-# change 1 dirname
-olddir_depth=1; newdir_depth=$olddir_depth; change_basename=false
+good_cases=(
+  # change basename
+  "path_a=file_a; path_b=file_b"
+  "path_a=dir/file_a; path_b=dir/file_b"
+  # change dirname
+  "path_a=dir_a/file; path_b=dir_b/file"
+  # move the file down
+  "path_a=file; path_b=dir/file"
+  "path_a=file; path_b=dir1/dir2/file"
+  "path_a=file; path_b=dir1/dir2/dir3/file"
+  # move the file down and change basename
+  "path_a=file_a; path_b=dir/file_b"
+  "path_a=file_a; path_b=dir1/dir2/file_b"
+  "path_a=file_a; path_b=dir1/dir2/dir3/file_b"
+)
 
-# no. this works as expected
-# move the file 1 level down. note: one depth must be 0
-olddir_depth=0; newdir_depth=1; change_basename=false
+bad_cases=(
+  # move the file up
+  "path_a=dir/file; path_b=file"
+  "path_a=dir1/dir2/file; path_b=file"
+  "path_a=dir1/dir2/dir3/file; path_b=file"
+  # move the file up and change basename
+  "path_a=dir/file_a; path_b=file_b"
+  "path_a=dir1/dir2/file_a; path_b=file_b"
+  "path_a=dir1/dir2/dir3/file_a; path_b=file_b"
+  # change 2 dirnames
+  "path_a=dir1a/dir2a/file; path_b=dir1b/dir2b/file"
+  # change 1 dirname and basename
+  "path_a=dir1a/file_a; path_b=dir1b/file_b"
+)
 
-# yes. this fails to dedupe "git mv"
-# move the file 1 level up. note: one depth must be 0
-olddir_depth=1; newdir_depth=0; change_basename=false
-
-# yes. this fails to dedupe "git mv"
-# change 2 dirnames
-olddir_depth=2; newdir_depth=$olddir_depth; change_basename=false
-
-# yes. this fails to dedupe "git mv"
-# change 1 dirname and basename
-olddir_depth=1; newdir_depth=$olddir_depth; change_basename=true
-
-
-
-# no effect
-const_path_prefix=
-#const_path_prefix=a/b/c/
-
-
-
-# no effect. same problem with copy
-move_or_copy=move
-#move_or_copy=copy
-
-
-
-use_text_files=false
-#use_text_files=true # TODO base64
-
-
-
-
-owner=milahu
-repo=git-bug-git-mv-wasteful-transfer-test
-
-remote=https://github.com/$owner/$repo
-
-echo "todo? create new repo \"$repo\" at https://github.com/new"
-
-# no effect
-num_files=1; file_size_mega=1
-num_files=1; file_size_mega=10 # make the blob transfer more noticable
-
-transfer_size_mega=$((num_files * file_size_mega))
-
-file_size=$((file_size_mega * 1024 * 1024))
-
-# no effect
-file_extension=.bin
-#file_extension=.txt
-
-# no effect
-disable_delta_compression=false
-#disable_delta_compression=true
-
-stop_git_push=false
-#stop_git_push=true # avoid large transfers
-
-set -e
-set -x
-
-# check dependencies
-command -v git
-command -v head # coreutils
-command -v unbuffer # expect
-
-function get_dir() {
-  local prefix=$1
-  local depth=$2
-  dir=
-  for d in $(seq 1 $depth); do
-    dir+=$prefix-$d/
-  done
-  echo $dir
+function git_pull() {
+  unbuffer git -C $dst_repo pull file://$src_repo main \
+  2>&1 | tr $'\r' $'\n' | grep -o -E "Unpacking objects.*done\." | cut -d' ' -f5-6
 }
 
-# mkdir fails if dir exists
-mkdir $repo
+# the file size must be much larger than the tree and commit objects
+# which have about 500 bytes, so the 500 bytes disappear by rounding
+# example: 1.000500 MB -> 1.00 MB
+file_size=$((1 * 1024 * 1024))
 
-cd $repo
-
-git init
-git remote add origin $remote
-
-echo "test" >readme.txt
-git add readme.txt
-git commit -m "add readme.txt"
-
-# disable delta compression
-if $disable_delta_compression; then
-  echo "*$file_extension -delta" >.gitattributes
-  git add .gitattributes
-  git commit -m "add .gitattributes"
-fi
-
-while true; do # retry loop
-  # force: replace old remote branch
-  git push origin -u main --force && break
-  echo "git push failed -> retrying"
-  sleep 10
+for test_case in "${good_cases[@]}" "${bad_cases[@]}"; do
+  src_repo=$PWD/src_repo; dst_repo=$PWD/dst_repo
+  mkdir -p $src_repo; mkdir -p $dst_repo
+  git -C $src_repo init -q; git -C $dst_repo init -q
+  eval "$test_case"
+  mkdir -p $src_repo/$(dirname $path_a)
+  mkdir -p $src_repo/$(dirname $path_b)
+  cat /dev/urandom | head -c$file_size >$src_repo/$path_a
+  git -C $src_repo add $path_a
+  git -C $src_repo commit -m "add file" -q
+  transfer_size_1=$(git_pull)
+  git -C $src_repo mv $path_a $path_b
+  git -C $src_repo commit -m "mv file" -q
+  transfer_size_2=$(git_pull)
+  if [[ "$transfer_size_1" == "$transfer_size_2" ]]
+  then echo "FAIL: $transfer_size_1 == $transfer_size_2 # $test_case"
+  else echo "pass: $transfer_size_1 != $transfer_size_2 # $test_case"
+  fi
+  rm -rf $src_repo; rm -rf $dst_repo
 done
 
-# create files
-for ((file_id=0; file_id<num_files; file_id++)); do
-  olddir=$(get_dir olddir-$file_id $olddir_depth)
-  oldname=oldname-$file_id$file_extension
-  oldpath=$const_path_prefix$olddir$oldname
-  mkdir -p $(dirname $oldpath)
-  # FIXME mkdir
-  if $use_text_files; then
-    cat /dev/urandom | base64 | head -c$file_size >$oldpath
-  else
-    cat /dev/urandom | head -c$file_size >$oldpath
-  fi
-  git add $oldpath
-done
-git commit -m "add oldname*"
-echo "expected: push $transfer_size_mega MByte"
-# Writing objects: 100% (12/12), 10.00 MiB | 789.00 KiB/s, done.
-while true; do # retry loop
-  git push origin -u main && break
-  echo "git push failed -> retrying"
-  sleep 10
-done
-
-const_path_prefix_depth=$(echo "$const_path_prefix" | tr -c -d / | wc -c)
-
-# move files
-# FIXME expected_num_transfer_trees is wrong in some cases
-expected_num_transfer_trees=0
-wrong_num_transfer_trees=0
-for ((file_id=0; file_id<num_files; file_id++)); do
-  olddir=$(get_dir olddir-$file_id $olddir_depth)
-  oldname=oldname-$file_id$file_extension
-  oldpath=$const_path_prefix$olddir$oldname
-  newdir=$(get_dir newdir-$file_id $newdir_depth)
-  if $change_basename; then
-    newname=newname-$file_id$file_extension
-    expected_num_transfer_trees=$((expected_num_transfer_trees + const_path_prefix_depth + newdir_depth))
-    # +1 for the extra blob object
-    wrong_num_transfer_trees=$((wrong_num_transfer_trees + const_path_prefix_depth + newdir_depth + 1))
-  else
-    newname=$oldname
-    # -1 because the last tree stays constant
-    expected_num_transfer_trees=$((expected_num_transfer_trees + const_path_prefix_depth + newdir_depth - 1))
-    # +1 for the extra blob object
-    wrong_num_transfer_trees=$((wrong_num_transfer_trees + const_path_prefix_depth + newdir_depth + 1 - 1))
-  fi
-  newpath=$const_path_prefix$newdir$newname
-  mkdir -p $(dirname $newpath)
-  case "$move_or_copy" in
-    move)
-      git mv $oldpath $newpath
-      ;;
-    copy)
-      cp $oldpath $newpath
-      git add $newpath
-      ;;
-  esac
-done
-# +1 for the root tree object
-# +1 for the commit object
-expected_num_transfer_objects=$((expected_num_transfer_trees + 2))
-wrong_num_transfer_objects=$((wrong_num_transfer_trees + 2))
-case "$move_or_copy" in
-  move)
-    git commit -m "mv oldname* newname*"
-    ;;
-  copy)
-    git commit -m "cp oldname* newname*"
-    ;;
-esac
-echo "expected: push about 1 KByte"
-echo "expected_num_transfer_objects: $expected_num_transfer_objects"
-echo "wrong_num_transfer_objects: $wrong_num_transfer_objects"
-
-# Writing objects: 100% (2/2), 388 bytes | 388.00 KiB/s, done.
-# this should take a few seconds to upload 1KB
-set +x
-found_transfer=false
-while read -r -d$'\r' line; do
-  echo "$line"
-  $found_transfer && continue
-  [[ "${line:0:16}" != "Writing objects:" ]] && continue
-  # line: 'Writing objects:  11% (5/42), 5.03 MiB | 1.86 MiB/s'
-  echo "# writing objects line: ${line@Q}"
-  num_transfer_objects=$(echo "$line" | sed -E 's|^Writing objects.*?% \([0-9]+/([0-9]+)\).*$|\1|')
-  echo "# num_transfer_objects: $num_transfer_objects"
-  if (( num_transfer_objects == expected_num_transfer_objects )); then
-    echo "# pass: $num_transfer_objects == $expected_num_transfer_objects"
-  else
-    echo "# fail: $num_transfer_objects != $expected_num_transfer_objects"
-  fi
-  found_transfer=true
-  if $stop_git_push; then
-    echo "stopping git push"; break
-  fi
-done < <(
-  while true; do # retry loop
-    unbuffer git push origin -u main 2>&1 && break
-    echo "git push failed -> retrying" >&2
-    sleep 10
-  done
-)
+cd ..
+rm -rf $tempdir
