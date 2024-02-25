7bf1529ccad810601baeef35c6661fd0c813d9ee
Milan Hauth <milahu@gmail.com>
2024-02-25 11:10:07 +0100

    up repro.sh

--- repro.sh
+++ repro.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# no. this works as expected
+#olddir_depth=0; newdir_depth=$olddir_depth
+
+# yes. this fails to dedupe "git mv"
+olddir_depth=1; newdir_depth=$olddir_depth
+
+
+
 owner=milahu
 repo=git-bug-git-mv-wasteful-transfer-test
 
@@ -7,95 +15,43 @@ remote=https://github.com/$owner/$repo
 
 echo "todo? create new repo \"$repo\" at https://github.com/new"
 
-
-
-# this was my original use case
-#num_files=275; file_size_mega=20
-
-# no. this works as expected
-#num_files=10; file_size_mega=1
-
-# no. this works as expected
-#num_files=10; file_size_mega=20
-
-# yes. this fails to dedupe "git mv"
-#num_files=10; file_size_mega=100
-cat >/dev/null <<'EOF'
-  remote: warning: See https://gh.io/lfs for more information.
-  remote: warning: File oldpath-4/oldto-4/olddir-4/oldname-4.bin is 100.00 MB; this is larger than GitHub's recommended maximum file size of 50.00 MB
-  remote: warning: GH001: Large files detected. You may want to try Git Large File Storage - https://git-lfs.github.com.
-
-  expected: push 1000 MByte
-  + git push origin -u main
-  Enumerating objects: 43, done.
-  Counting objects: 100% (43/43), done.
-  Delta compression using up to 4 threads
-  Compressing objects: 100% (2/2), done.
-  Writing objects: 100% (42/42), 1000.08 MiB | 466.00 KiB/s, done.
-  Total 42 (delta 0), reused 0 (delta 0), pack-reused 0
-
-  expected: push about 1 KByte
-  + git push origin -u main
-  Enumerating objects: 43, done.
-  Counting objects: 100% (43/43), done.
-  Delta compression using up to 4 threads
-  Compressing objects: 100% (2/2), done.
-  Writing objects: 100% (42/42), 1000.08 MiB | 1.56 MiB/s, done.
-  Total 42 (delta 0), reused 0 (delta 0), pack-reused 0
-EOF
-
-# yes. this fails to dedupe "git mv"
-#num_files=50; file_size_mega=20
-cat >/dev/null <<'EOF'
-  expected: push 1000 MByte
-  + git push origin -u main
-  Enumerating objects: 203, done.
-  Counting objects: 100% (203/203), done.
-  Delta compression using up to 4 threads
-  Compressing objects: 100% (2/2), done.
-  Writing objects: 100% (202/202), 1000.09 MiB | 770.00 KiB/s, done.
-  Total 202 (delta 0), reused 0 (delta 0), pack-reused 0
-
-  expected: push about 1 KByte
-  + git push origin -u main
-  Enumerating objects: 203, done.
-  Counting objects: 100% (203/203), done.
-  Delta compression using up to 4 threads
-  Compressing objects: 100% (2/2), done.
-  Writing objects:   2% (5/202), 17.02 MiB | 891.00 KiB/s
-EOF
-
-# work in progress ...
-num_files=10; file_size_mega=50
-
-# TODO find the limit of git or github
-# when does it start failing to dedupe the blob objects?
-
-# FIXME at some point, "git push" will start failing
-cat >/dev/null <<'EOF'
-  $ git push
-  Enumerating objects: 293, done.
-  Counting objects: 100% (293/293), done.
-  Delta compression using up to 4 threads
-  Compressing objects: 100% (14/14), done.
-  error: RPC failed; HTTP 500 curl 22 The requested URL returned error: 500
-  send-pack: unexpected disconnect while reading sideband packet
-  Writing objects: 100% (290/290), 5.34 GiB | 2.89 MiB/s, done.
-  Total 290 (delta 5), reused 0 (delta 0), pack-reused 0
-  fatal: the remote end hung up unexpectedly
-  Everything up-to-date
-EOF
+# no effect
+num_files=1; file_size_mega=1
 
 transfer_size_mega=$((num_files * file_size_mega))
 
 file_size=$((file_size_mega * 1024 * 1024))
 
+# no effect
+file_extension=.bin
+#file_extension=.txt
+
+# no effect
+disable_delta_compression=false
+#disable_delta_compression=true
+
+# debug
 continue_move_files=false
 # continue an interrupted run after the first "git push"
 #continue_move_files=true
 
 set -e
 set -x
+
+# check dependencies
+command -v git
+command -v head # coreutils
+command -v unbuffer # expect
+
+function get_dir() {
+  local prefix=$1
+  local depth=$2
+  dir=
+  for d in $(seq 1 $depth); do
+    dir+=$prefix-$d/
+  done
+  echo $dir
+}
 
 if ! $continue_move_files; then
 # mkdir fails if dir exists
@@ -109,41 +65,86 @@ if ! $continue_move_files; then
 git init
 git remote add origin $remote
 
-# disable delta compression
-echo "*.bin -delta" >.gitattributes
-git add .gitattributes
-git commit -m init
+echo "test" >readme.txt
+git add readme.txt
+git commit -m "add readme.txt"
 
-# force: replace old remote branch
-git push origin -u main --force
+# disable delta compression
+if $disable_delta_compression; then
+echo "*$file_extension -delta" >.gitattributes
+git add .gitattributes
+git commit -m "add .gitattributes"
+fi
+
+while true; do # retry loop
+  # force: replace old remote branch
+  git push origin -u main --force && break
+  echo "git push failed -> retrying"
+  sleep 10
+done
 
 # create files
 for ((file_id=0; file_id<num_files; file_id++)); do
-  olddir=oldpath-$file_id/oldto-$file_id/olddir-$file_id
-  mkdir -p $olddir
-  oldpath=$olddir/oldname-$file_id.bin
+  olddir=$(get_dir olddir-$file_id $olddir_depth)
+  [ -n "$olddir" ] && mkdir -p $olddir
+  oldpath=${olddir}oldname-$file_id$file_extension
   head -c$file_size /dev/urandom >$oldpath
   git add $oldpath
 done
 git commit -m "add oldname*"
 echo "expected: push $transfer_size_mega MByte"
 # Writing objects: 100% (12/12), 10.00 MiB | 789.00 KiB/s, done.
-git push origin -u main
+while true; do # retry loop
+  git push origin -u main && break
+  echo "git push failed -> retrying"
+  sleep 10
+done
 
 fi # end of: if ! $continue_move_files
 
 # move files
+# 2 = commit object + root tree object
+expected_num_transfer_objects=2
+wrong_num_transfer_objects=2
 for ((file_id=0; file_id<num_files; file_id++)); do
-  olddir=oldpath-$file_id/oldto-$file_id/olddir-$file_id
-  oldpath=$olddir/oldname-$file_id.bin
-  newdir=newpath-$file_id/newto-$file_id/newdir-$file_id
-  newpath=$newdir/newname-$file_id.bin
-  mkdir -p $newdir
+  olddir=$(get_dir olddir-$file_id $olddir_depth)
+  oldpath=${olddir}oldname-$file_id$file_extension
+  newdir=$(get_dir newdir-$file_id $newdir_depth)
+  expected_num_transfer_objects=$((expected_num_transfer_objects + newdir_depth))
+  # +1 for the extra blob object
+  wrong_num_transfer_objects=$((wrong_num_transfer_objects + newdir_depth + 1))
+  newpath=${newdir}newname-$file_id$file_extension
+  [ -n "$newdir" ] && mkdir -p $newdir
   git mv $oldpath $newpath
 done
 git commit -m "mv oldname* newname*"
 echo "expected: push about 1 KByte"
+echo "expected_num_transfer_objects: $expected_num_transfer_objects"
+echo "wrong_num_transfer_objects: $wrong_num_transfer_objects"
+
 # Writing objects: 100% (2/2), 388 bytes | 388.00 KiB/s, done.
 # this should take a few seconds to upload 1KB
-timeout 120 \
-git push origin -u main
+set +x
+found_transfer=false
+while read -r -d$'\r' line; do
+  echo "$line"
+  $found_transfer && continue
+  [[ "${line:0:16}" != "Writing objects:" ]] && continue
+  # line: 'Writing objects:  11% (5/42), 5.03 MiB | 1.86 MiB/s'
+  echo "# writing objects line: ${line@Q}"
+  num_transfer_objects=$(echo "$line" | sed -E 's|^Writing objects.*?% \([0-9]+/([0-9]+)\).*$|\1|')
+  echo "# num_transfer_objects: $num_transfer_objects"
+  if (( num_transfer_objects == expected_num_transfer_objects )); then
+    echo "# pass: $num_transfer_objects == $expected_num_transfer_objects"
+  else
+    echo "# fail: $num_transfer_objects != $expected_num_transfer_objects"
+  fi
+  found_transfer=true
+  echo "stopping git push"; break
+done < <(
+  while true; do # retry loop
+    unbuffer git push origin -u main 2>&1 && break
+    echo "git push failed -> retrying" >&2
+    sleep 10
+  done
+)
