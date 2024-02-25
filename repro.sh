62a1af108881f1123f41b702a318fc39f78fa138
Milan Hauth <milahu@gmail.com>
2024-02-25 08:49:58 +0100

    up repro.sh

--- repro.sh
+++ repro.sh
@@ -12,14 +12,40 @@ echo "todo? create new repo \"$repo\" at https://github.com/new"
 # this was my original use case
 #num_files=275; file_size_mega=20
 
-# ok, this works as expected
+# no. this works as expected
 #num_files=10; file_size_mega=1
 
-# ok, this works as expected
+# no. this works as expected
 #num_files=10; file_size_mega=20
 
-# work in progress...
-num_files=10; file_size_mega=100
+# yes. this fails to dedupe "git mv"
+#num_files=10; file_size_mega=100
+cat >/dev/null <<'EOF'
+  remote: warning: See https://gh.io/lfs for more information.
+  remote: warning: File oldpath-4/oldto-4/olddir-4/oldname-4.bin is 100.00 MB; this is larger than GitHub's recommended maximum file size of 50.00 MB
+  remote: warning: GH001: Large files detected. You may want to try Git Large File Storage - https://git-lfs.github.com.
+
+  expected: push 1000 MByte
+  + git push origin -u main
+  Enumerating objects: 43, done.
+  Counting objects: 100% (43/43), done.
+  Delta compression using up to 4 threads
+  Compressing objects: 100% (2/2), done.
+  Writing objects: 100% (42/42), 1000.08 MiB | 466.00 KiB/s, done.
+  Total 42 (delta 0), reused 0 (delta 0), pack-reused 0
+
+  expected: push about 1 KByte
+  + git push origin -u main
+  Enumerating objects: 43, done.
+  Counting objects: 100% (43/43), done.
+  Delta compression using up to 4 threads
+  Compressing objects: 100% (2/2), done.
+  Writing objects: 100% (42/42), 1000.08 MiB | 1.56 MiB/s, done.
+  Total 42 (delta 0), reused 0 (delta 0), pack-reused 0
+EOF
+
+# work in progress ...
+num_files=50; file_size_mega=20
 
 # TODO find the limit of git or github
 # when does it start failing to dedupe the blob objects?
@@ -39,15 +65,25 @@ cat >/dev/null <<'EOF'
   Everything up-to-date
 EOF
 
+transfer_size_mega=$((num_files * file_size_mega))
 
+file_size=$((file_size_mega * 1024 * 1024))
+
+continue_move_files=false
+# continue an interrupted run after the first "git push"
+#continue_move_files=true
 
 set -e
 set -x
 
+if ! $continue_move_files; then
 # mkdir fails if dir exists
 mkdir $repo
+fi
 
 cd $repo
+
+if ! $continue_move_files; then
 
 git init
 git remote add origin $remote
@@ -59,10 +95,6 @@ git commit -m init
 
 # force: replace old remote branch
 git push origin -u main --force
-
-transfer_size_mega=$((num_files * file_size_mega))
-
-file_size=$((file_size_mega * 1024 * 1024))
 
 # create files
 for ((file_id=0; file_id<num_files; file_id++)); do
@@ -77,12 +109,15 @@ echo "expected: push $transfer_size_mega MByte"
 # Writing objects: 100% (12/12), 10.00 MiB | 789.00 KiB/s, done.
 git push origin -u main
 
+fi # end of: if ! $continue_move_files
+
 # move files
 for ((file_id=0; file_id<num_files; file_id++)); do
-  newdir=newpath-$file_id/newto-$file_id/newdir-$file_id
-  mkdir -p $newdir
+  olddir=oldpath-$file_id/oldto-$file_id/olddir-$file_id
   oldpath=$olddir/oldname-$file_id.bin
+  newdir=newpath-$file_id/newto-$file_id/newdir-$file_id
   newpath=$newdir/newname-$file_id.bin
+  mkdir -p $newdir
   git mv $oldpath $newpath
 done
 git commit -m "mv oldname* newname*"
